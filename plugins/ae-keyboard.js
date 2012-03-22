36b2d187943ad8c48223969d34a1890ab2215cce
Xavier Laumonier <xlaumonier@gmail.com>
2012-03-22 10:27:35 +0100

    Dev commit

---
+++ plugins/ae-keyboard.js
@@ -0,0 +1,23 @@
+(function(){
+
+	if ( !window.ajsf )
+	{
+		return;
+	}
+
+
+	ajsf.keyboard = {
+		// taken from http://www.bytemycode.com/snippets/snippet/771/
+		getEventCode: function (e)
+		{
+			if (e.keyCode) )
+			{
+				
+			}
+		}
+	} ;
+
+
+
+
+})();
